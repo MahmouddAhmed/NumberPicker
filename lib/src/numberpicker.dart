2bcc9663c83e38f6cfa177659f38c1f1f8005e08
Mahmouddahmed <mahmodahmed98.ma@gmail.com>
2022-03-01 18:04:14 +0200

    decoration is now behind selected number

--- lib/src/numberpicker.dart
+++ lib/src/numberpicker.dart
@@ -165,6 +165,11 @@ class _NumberPickerState extends State<NumberPicker> {
         },
         child: Stack(
           children: [
+            _NumberPickerSelectedItemDecoration(
+              axis: widget.axis,
+              itemExtent: itemExtent,
+              decoration: widget.decoration,
+            ),
             if (widget.infiniteLoop)
               InfiniteListView.builder(
                 scrollDirection: widget.axis,
@@ -182,11 +187,6 @@ class _NumberPickerState extends State<NumberPicker> {
                 itemBuilder: _itemBuilder,
                 padding: EdgeInsets.zero,
               ),
-            _NumberPickerSelectedItemDecoration(
-              axis: widget.axis,
-              itemExtent: itemExtent,
-              decoration: widget.decoration,
-            ),
           ],
         ),
       ),
